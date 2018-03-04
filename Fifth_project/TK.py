4eccc1edf748c4ee098e70b2a7b663c05b8485d2
Mohammad Mahjoub <mmahjoub@westmont.edu>
2018-03-03 21:32:07 -0800

    making progess

--- Fifth_project/TK.py
+++ Fifth_project/TK.py
@@ -4,7 +4,9 @@ from tkinter.messagebox import showerror
 
 root = Tk()
 root.title("File Counter ")
-root.geometry('300x300')  # set up the size
+color = 'black'
+root.geometry('500x500')  # set up the size
+root.configure(bg=color)
 root.resizable(width=False, height=False)
 
 
@@ -12,43 +14,45 @@ first_f_count = IntVar()
 first_s_count = IntVar()
 words = StringVar()
 count_result = dict()
+txt_files = dict()
 
 
-def first_file():
-    return count(askopenfilename())
+def create_file(file_number):
+    txt_files[file_number] = askopenfilename()
 
 
-def second_file():
-    return count(askopenfilename())
-
-
-def count(file):
-    if file:
-        try:
-            file = open(str(file), 'r')
-            full_text = file.readline()
-            file.close()
-            for word in words_list.get().split(','):
-                for text in full_text:
-                    if word in count_result:
-                        count_result[word] = text.count(word)
-                    else:
-                        count_result[word] = count_result[word].get() + text.count(word)
-            print(count_result)
-        except:
-            showerror("Open Source File", "Failed to read file\n'%s'" % file)
-            return
+def count():
+    for k, v in txt_files.items():
+        if v:
+            try:
+                file = open(str(v), 'r')
+                full_text = file.readlines()
+                file.close()
+                for word in words_list.get().split(','):
+                    for text in full_text:
+                        if word in count_result:  # mo
+                            count_result[word] = count_result[word].get() + text.count(word)
+                        else:
+                            count_result[word] = text.count(word)
+                print(count_result)
+            except:
+                pass
 
 
 words_list = Entry(root, width=40, bd=2, insertwidth=2)
-words_list.pack(fill=X, padx=5, expand=True)
+words_list.pack(fill=X)
 
-first_f = Button(root, text="First File ", command=lambda: first_file())
-first_f.place(x=0, y=0)
+first_f = Button(root, text="First File ", highlightbackground=color, command=lambda: create_file(1))
+first_f.place(x=0, y=30)
 
-second_f = Button(root, text="Second File ", command=lambda: second_file())
-second_f.place(x=0, y=23)
+second_f = Button(root, text="Second File ", highlightbackground=color, command=lambda: create_file(2))
+second_f.place(x=0, y=60)
 
+third_f = Button(root, text="Third File ", highlightbackground=color, command=lambda: create_file(3))
+third_f.place(x=0, y=90)
+
+count_txt = Button(root, text="Count Files ", highlightbackground=color, command=lambda: count())
+count_txt.place(x=400, y=90)
 
 
 root.mainloop()
